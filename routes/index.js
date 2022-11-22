dc633428a5dc3ad0788f57a578dd3cf2f7dee9aa
Curriculo <vegasangel51@gmail.com>
2022-11-22 14:21:16 -0400

    nuevo proyecto

--- routes/index.js
+++ routes/index.js
@@ -3,28 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  let nombre = "Angel Gabriel"
-    let apellido = "Vegas Moreno"
-  let cedula = "23.951.923"
-   let seccion = "1"
-  res.render('index', {
-    title: 'UNERG',
-    name: nombre,
-      apellido,
-    cedula,
-   seccion,
-  });
+  res.render('index', { 
+   title: 'Curiculum CV'})
 });
 
-router.get('/otro', function(req, res, next) {
-  let nombre = "Vegas"
-  let cedula = "23.951.923"
-  
-  res.render('index', {
-    title: 'UNERG',
-    name: nombre,
-    cedula,
-  });
-});
+
 
 module.exports = router;
